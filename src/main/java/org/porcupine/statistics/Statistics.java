0bfd66ffd40785c50b3f8b082dc33fba597caecb
Milan Dierick <milan.dierick@gmail.com>
2023-01-11 16:06:18 +0100

    Updated TODO in Statistics.java

--- src/main/java/org/porcupine/statistics/Statistics.java
+++ src/main/java/org/porcupine/statistics/Statistics.java
@@ -7,13 +7,12 @@ package org.porcupine.statistics;
 /**
  * This interface is used as a wrapper for all statistics.
  *
+ * @author Milan Dierick
  * @implNote Internally, this interface uses a HashSet to store all statistic objects. This is done to allow for other
  * statistics to be added at runtime, for example by other modules. The framework provides accessors for all vanilla
  * statistics, but other statistics can be added using the {@link #put(Class, IStats)} method.
- *
- * @author Milan Dierick
  */
-// TODO: The statistics need to be saved and loaded. This is not yet implemented.
+// TODO: The statistics need to be saved and loaded, as to not having to recalculate statistics when loading the game.
 @SuppressWarnings("ClassNamePrefixedWithPackageName")
 public final class Statistics {
 	private static final StatisticsCategoryCache cache = new StatisticsCategoryCache();
@@ -30,7 +29,7 @@ public final class Statistics {
 	}
 	
 	/**
-	 * @param key   The class of the statistic to get.
+	 * @param key   The class type of the statistics object to insert.
 	 * @param value The statistic to put.
 	 */
 	public static void put(Class<? extends IStats> key, IStats value) {
