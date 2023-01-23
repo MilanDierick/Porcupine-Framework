817fab5832850b9234b4f72a5f0012302403a9f0
Milan Dierick <milan.dierick@gmail.com>
2023-01-23 22:56:50 +0100

    Documentation

--- src/main/java/org/porcupine/events/IRoomEvent.java
+++ src/main/java/org/porcupine/events/IRoomEvent.java
@@ -7,8 +7,16 @@ package org.porcupine.events;
 import settlement.room.main.Room;
 
 public interface IRoomEvent {
+	/**
+	 * This method is called when the event occurs.
+	 *
+	 * @param room the room that this event is associated with.
+	 */
 	void onEvent(Room room);
 	
+	/**
+	 * @return the chance that this event will occur per in-game second.
+	 */
 	float chancePerSecond();
 	
 	/**
